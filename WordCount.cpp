506c4a209e167c54fd2afefe0fa3edc4878c4dfd
acizas <acizas@ucsb.edu>
2020-02-12 23:57:51 -0800

    Partially complete

--- WordCount.cpp
+++ WordCount.cpp
@@ -2,6 +2,9 @@
 
 #include "WordCount.h"
 #include <vector>
+#include <utility>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -23,24 +26,25 @@ int WordCount::getTotalWords() const {
   // Reads through every vector in the hash table
   for (int i = 0; i < 100; i++) {
     // Checks to see if the spot in the table is occupied
-    if (table[i] != NULL) {
-      // Reads through every word in the vector
-      //for (int j = 0; j < (table[i]).size(); j++) {
-      //totalWords++;
-      //}
-      totalWords =+ (table[i]).size();
+    if (!(table[i].empty())){
+      for (int j = 0; j < table[i].size(); j++) {
+	if ((table[i][j]).second != 0)
+	totalWords += table[i][j].second;
+      }
     }
   }
   return totalWords;
 }
 
-// Counts through every element of the table and adds the number of unique words
 int WordCount::getNumUniqueWords() const {
   // If a vector in the table contains at least one word, it adds to the total
   int numUniqueWords = 0;
   for (int i = 0; i < 100; i++) {
-    if (table[i] != NULL)
-      numUniqueWords++;
+    if (!(table[i].empty())){
+      for (int j = 0; j < table[i].size(); j++) {
+	numUniqueWords += 1;
+      }
+    }
   }
   return numUniqueWords;
     
@@ -53,18 +57,18 @@ int WordCount::getWordCount(std::string word) const {
   std::string validWord = makeValidWord(word);
   int hashKey = hash(validWord);
   // If the word is not present, returns 0 
-  if (table[hashKey] == NULL) {
+  if (table[hashKey].empty()) {
     return 0;
   }
   else {
     // Counts through the elements in the vector at the word's hash table index
-    for (int i = 0; i < (table[hashKey]).size(); i++) {
-      wordCount++;
+    for (int i = 0; i < table[hashKey].size(); i++) {
+      if (table[hashKey][i].first == validWord)
+	  wordCount = table[hashKey][i].second;
     }
   }
-  returns wordCount;
-}
-	
+  return wordCount;
+}	
 int WordCount::incrWordCount(std::string word) {
 
   int wordCount = 0;
@@ -72,19 +76,16 @@ int WordCount::incrWordCount(std::string word) {
   std::string validWord = makeValidWord(word);
   // Checks to see if the word is in the table
   int hashKey = hash(validWord);
-  if (table[hashKey] != NULL) {
+  if (!(table[hashKey].empty())) {
     // Adds to the existing linked list and increments the word count
-    (table[hashKey]).push_back(validWord);
-    //for (int i = 0; i < (table[hashKey]).size(); i++){
-    //wordCount++;
-    //}
-    wordCount = (table[hashKey]).size();
+    table[hashKey].second += 1;
+    wordCount = table[hashKey].second;
+    ;
   }
   else {
-    // Creates a word linked list on the table and increments the word count
-    vector<word> newWord;
+    // Creates an entry in the table for the word and returns a word count of 1
+    std::vector<std::pair<word, 1>> newWord;
     table[hashKey] = newWord;
-    (table[hashKey]).push_back(validWord);
     wordCount = 1;
   }
   return wordCount;
@@ -97,25 +98,42 @@ int WordCount::decrWordCount(std::string word) {
   std::string validWord = makeValidWord(word);
   int hashKey = hash(validWord);
   // Checks to see if the word is already in the table
-  if (table[hashKey] != NULL) {
+  for (int i = 0; i < table[hashKey].size(); i++) {
+    if (table[hashKey][i].first == validWord) {
+      int number = table[hashKey][i].second;
+      if (number > 1) {
+	table[hashKey][i].second -= 1;
+	wordCount = table[hashKey][i].second;
+      }
+      else {
+	table[hashKey][i].second -= 1;
+	wordCount = 0;
+      }
+    }
+    return wordCount;
+  }
+  else {
+    wordCount = -1;
+    return wordCount;
+  }
+}
+    
+  if (!(table[hashKey].empty())) {
     // If there is more than 1 instance of the word
-    if ((table[hashKey]).size() > 1) {
-      (table[hashKey]).pop_back();
-      // for (int i = 0; i < (table[hashKey]).size(); i++){
-      //wordCount++;
-      //}
-      wordCount = (table[hashKey]).size();
+    if ((table[hashKey].second) > 1) {
+      table[hashKey].second - 1;
+      wordCount = table[hashKey].second;
     }
     // If there is only 1 instance of the word
     else {
-      (table[hashKey]).pop_back();
+      table[hashKey].second - 1;
       wordCount = 0;
     }
   }
   // If there is no instance of the word, returns -1
   else {
     wordCount = -1
-  }
+      }
   return wordCount;
 }
 
